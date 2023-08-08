cf8cd76003b645fb7e0d0d38298cd63caac5b802
Daniel Liwonde <dliwonde@biu-edu.com>
2023-08-08 22:47:56 +0200

    changed icon on menu to box-open

--- ttopMenu.php
+++ ttopMenu.php
@@ -6,8 +6,8 @@
         &nbsp; Lecturer Timetable</a>&nbsp;
     <a href="examTT.php" class="btn btn-info"> <i class="fas fa-edit"></i>
         &nbsp; Exam Timetable</a>
-    <a href="examTTsup.php" class="btn btn-info"><i class="fas fa-redo"></i>
-        &nbsp; Create Special Timetable</a>
+    <a href="examTTsup.php" class="btn btn-info"><i class="fas fa-box-open"></i>
+        &nbsp; Special Timetable</a>
     <a href="ttSetings.php" class="btn btn-info"><i class="fas fa-gear"></i>
         &nbsp; Settings</a>
 </span>
