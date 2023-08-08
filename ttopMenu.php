658f363a350c06ec482592979cc8b89add88f277
Daniel D Liwonde <44372690+daniel-liwonde@users.noreply.github.com>
2023-08-08 22:25:53 +0200

    Update ttopMenu.php
    changed supp to create special timetable on the menu

--- ttopMenu.php
+++ ttopMenu.php
@@ -7,7 +7,7 @@
     <a href="examTT.php" class="btn btn-info"> <i class="fas fa-edit"></i>
         &nbsp; Exam Timetable</a>
     <a href="examTTsup.php" class="btn btn-info"><i class="fas fa-redo"></i>
-        &nbsp; Supp Timetable</a>
+        &nbsp; Create Special Timetable</a>
     <a href="ttSetings.php" class="btn btn-info"><i class="fas fa-gear"></i>
         &nbsp; Settings</a>
 </span>
